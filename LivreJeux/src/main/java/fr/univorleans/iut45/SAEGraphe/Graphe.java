65a42b56d49b1e4f56b30bb1c04aa93061e7a794
Remi-Bens <remi.bens@etu.univ-orleans.fr>
2026-05-13 12:11:07 +0200

    Ajout de fonctions de Graphe

--- LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/Graphe.java
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/Graphe.java
@@ -1,5 +1,32 @@
 package fr.univorleans.iut45.SAEGraphe;
 
+import org.jgrapht.Graph;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Graphe {
     
+    private Graph<Page,DefaultEdge> graphe;
+    private List<Objet> inventaire;
+
+    public Graphe(){
+
+        this.graphe = new Graph();
+        this.inventaire = new ArrayList<>();
+    }
+
+    public void start(){
+        
+    }
+
+    public boolean tousObjets(){
+
+    }
+
+    public void explorePage(Page page){
+
+    }
+
+    
+
 }
