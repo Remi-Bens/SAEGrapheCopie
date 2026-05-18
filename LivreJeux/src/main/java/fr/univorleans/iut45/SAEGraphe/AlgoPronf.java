d14e7468a5d136d8103e0a72b3c80cbf300e65d6
antoinefontaine <antoine.fontaine1@etu.univ-orleans.fr>
2026-05-18 16:55:50 +0200

    Alpha de l'algo en profondeur

--- LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoPronf.java
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoPronf.java
@@ -10,4 +10,19 @@ public class AlgoPronf {
         this.pile = new Stack<>();
         this.gr = g;
     }
+
+     public boolean start(){
+        Page prem = gr.premierePage();
+        for(Page p : gr.getGraphe().successorListOf(prem)){
+            pile.push(p);}
+        
+        while(!pile.isEmpty()){
+            Page suiv = pile.pop();
+            suiv.lecture();
+            System.out.println("La page n° "+suiv.getNum()+" est lu !");
+            for(Page p : gr.getGraphe().successorListOf(suiv)){
+                if(!p.estLu()){
+            pile.push(p);}}
+        }
+    }
 }
