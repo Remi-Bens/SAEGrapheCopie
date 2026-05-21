fcecbb65324f478e99f679f6ca7b99b0259f6aea
antoinefontaine <antoine.fontaine1@etu.univ-orleans.fr>
2026-05-21 12:02:27 +0200

    AlgoLarge complété

--- LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoLarge.java
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoLarge.java
@@ -1,14 +1,38 @@
 package fr.univorleans.iut45.SAEGraphe;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jgrapht.graph.DefaultEdge;
+
+import java.util.ArrayList;
 
 public class AlgoLarge {
-    Stack<Page> pile;
-Graphe gr;
+    List<Page> liste;
+    Graphe gr;
 
 public AlgoLarge(Graphe g){
-this.pile = new Stack<>();
-this.gr = g;
-}
+    this.liste = new ArrayList<>();
+    this.gr = g;
+    }
+
+    public boolean start(){
+    Page prem = gr.premierePage();
+    prem.lire();
+    for(DefaultEdge e : gr.getGraphe().outgoingEdgesOf(prem)){
+        Page p = this.gr.getGraphe().getEdgeTarget(e);
+        liste.add(p);}
+    while(!liste.isEmpty()){
+        Page suiv = liste.get(0);
+        suiv.lire();
+        System.out.println("La page n° "+suiv.getNum()+" est lu !");
+        for(DefaultEdge edge : gr.getGraphe().outgoingEdgesOf(suiv)){
+            Page target = gr.getGraphe().getEdgeTarget(edge);
+            if(!target.estLue()){
+            liste.add(target);}
+            }
+        }
+    return true;
+    }
 
 }
