fb968448ad6677c61c0cdd79a23e5512bc3ef8e6
antoinefontaine <antoine.fontaine1@etu.univ-orleans.fr>
2026-05-21 11:12:16 +0200

    corection packatge

--- LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoPronf.java
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoPronf.java
@@ -1,5 +1,5 @@
+package fr.univorleans.iut45.SAEGraphe;
 import java.util.Stack;
-
 import org.jgrapht.graph.DefaultEdge;
 
 public class AlgoPronf {
@@ -12,22 +12,22 @@ this.gr = g;
 }
 
 public boolean start(){
-Page prem = gr.premierePage();
-prem.lire();
-for(DefaultEdge e : gr.getGraphe().outgoingEdgesOf(prem)){
-Page p = this.gr.getGraphe().getEdgeTarget(e);
-pile.push(p);}
-while(!pile.isEmpty()){
-Page suiv = pile.pop();
-suiv.lire();
-System.out.println("La page n° "+suiv.getNum()+" est lu !");
-for(DefaultEdge edge : gr.getGraphe().outgoingEdgesOf(suiv)){
-Page target = gr.getGraphe().getEdgeTarget(edge);
-if(!target.estLue()){
-pile.push(target);}
-}
-}
-return true;
-}
+    Page prem = gr.premierePage();
+    prem.lire();
+    for(DefaultEdge e : gr.getGraphe().outgoingEdgesOf(prem)){
+        Page p = this.gr.getGraphe().getEdgeTarget(e);
+        pile.push(p);}
+    while(!pile.isEmpty()){
+        Page suiv = pile.pop();
+        suiv.lire();
+        System.out.println("La page n° "+suiv.getNum()+" est lu !");
+        for(DefaultEdge edge : gr.getGraphe().outgoingEdgesOf(suiv)){
+            Page target = gr.getGraphe().getEdgeTarget(edge);
+            if(!target.estLue()){
+            pile.push(target);}
+            }
+        }
+    return true;
+    }
 }
 
