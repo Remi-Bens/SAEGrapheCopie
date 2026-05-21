735c0ddb021ca4d770c73332403198bb61e65317
antoinefontaine <antoine.fontaine1@etu.univ-orleans.fr>
2026-05-21 11:57:59 +0200

    AlgoLarge

---
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoLarge.java
@@ -0,0 +1,14 @@
+package fr.univorleans.iut45.SAEGraphe;
+
+import java.util.Stack;
+
+public class AlgoLarge {
+    Stack<Page> pile;
+Graphe gr;
+
+public AlgoLarge(Graphe g){
+this.pile = new Stack<>();
+this.gr = g;
+}
+
+}
