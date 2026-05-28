d4377872d4a478f2a89f6ae6d1db4719d3e40c50
Remi-Bens <remi.bens@etu.univ-orleans.fr>
2026-05-28 13:49:24 +0200

    Correction du parcours en largeur

--- LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoLarge.java
+++ LivreJeux/src/main/java/fr/univorleans/iut45/SAEGraphe/AlgoLarge.java
@@ -3,34 +3,46 @@ package fr.univorleans.iut45.SAEGraphe;
 import java.util.ArrayList;
 import java.util.List;
 import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultDirectedGraph;
 import java.util.ArrayList;
 
 public class AlgoLarge {
     List<Page> liste;
-    Graphe gr;
+    DefaultDirectedGraph<Page,DefaultEdge> gr;
 
-public AlgoLarge(Graphe g){
+public AlgoLarge(DefaultDirectedGraph<Page,DefaultEdge> g){
     this.liste = new ArrayList<>();
     this.gr = g;
     }
 
     public boolean start(){
-    Page prem = gr.premierePage();
+    Page prem = new Page();
+    for (Page page:gr.vertexSet()){
+        if (page.getDeb()) {
+            prem = page;
+            break;
+        }
+    }
+
     prem.lire();
-    for(DefaultEdge e : gr.getGraphe().outgoingEdgesOf(prem)){
-        Page p = this.gr.getGraphe().getEdgeTarget(e);
+    
+    for(DefaultEdge e : gr.outgoingEdgesOf(prem)){
+        Page p = this.gr.getEdgeTarget(e);
         liste.add(p);}
     while(!liste.isEmpty()){
-        Page suiv = liste.get(0);
+        Page suiv = liste.remove(0);
         suiv.lire();
         System.out.println("La page n° "+suiv.getNum()+" est lu !");
-        for(DefaultEdge edge : gr.getGraphe().outgoingEdgesOf(suiv)){
-            Page target = gr.getGraphe().getEdgeTarget(edge);
+        for(DefaultEdge edge : gr.outgoingEdgesOf(suiv)){
+            Page target = gr.getEdgeTarget(edge);
             if(!target.estLue()){
             liste.add(target);}
             }
+        if(suiv.getFin()){
+                return true;
         }
-    return true;
+        }
+        return false;
     }
 
 }
